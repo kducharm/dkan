3d009960b68629a683784340b6f2b8fccc4e4541
Kristian Ducharme <kducharm@3072643.no-reply.drupal.org>
2017-06-20 13:12:59 -0700

    Removed unnecessary settings.php updating from drupal-rebuild.sh

--- .ahoy/.scripts/drupal-rebuild.sh
+++ .ahoy/.scripts/drupal-rebuild.sh
@@ -16,9 +16,6 @@ drush --root=docroot -y --verbose si minimal --sites-subdir=default --account-pa
   ln -s ../../dkan docroot/profiles/dkan
 chmod +w docroot/sites/default/settings.php
 
-if [ "$AHOY_CMD_PROXY" = "DOCKER" ]; then
-  printf "// Docker Database Settings\n\$databases['default']['default'] = array(\n  'database' => 'drupal',\n  'username' => 'drupal',\n  'password' => '123',\n  'host' => 'db',\n  'port' => '',\n  'driver' => 'mysql',\n  'prefix' => '',\n);\n" >> docroot/sites/default/settings.php
-fi
 printf "// DKAN Datastore Fast Import options.\n\$databases['default']['default']['pdo'] = array(\n  PDO::MYSQL_ATTR_LOCAL_INFILE => 1,\n  PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => 1,\n);\n" >> docroot/sites/default/settings.php
 
 chmod -w docroot/sites/default/settings.php
